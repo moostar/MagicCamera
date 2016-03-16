be4bf555ed6b20452aabd1a6d1b18335c27eb9a6
陈世超 <shichao.chen@dpocket.cn>
2016-03-16 16:44:29 +0800

    增加音频

--- Project-AndroidStudio/magicfilter/src/main/java/com/seu/magicfilter/widget/MagicCameraView.java
+++ Project-AndroidStudio/magicfilter/src/main/java/com/seu/magicfilter/widget/MagicCameraView.java
@@ -14,15 +14,19 @@ import com.seu.magicfilter.camera.CameraEngine;
 import com.seu.magicfilter.camera.utils.CameraInfo;
 import com.seu.magicfilter.filter.base.MagicCameraInputFilter;
 import com.seu.magicfilter.filter.helper.MagicFilterType;
+import com.seu.magicfilter.helper.SavePictureTask;
 import com.seu.magicfilter.utils.MagicParams;
 import com.seu.magicfilter.utils.OpenGlUtils;
 import com.seu.magicfilter.utils.Rotation;
 import com.seu.magicfilter.utils.TextureRotationUtil;
-import com.seu.magicfilter.helper.SavePictureTask;
-import com.seu.magicfilter.video.TextureMovieEncoder;
+import com.seu.magicfilter.video.MediaAudioEncoder;
+import com.seu.magicfilter.video.MediaEncoder;
+import com.seu.magicfilter.video.MediaMuxerWrapper;
+import com.seu.magicfilter.video.MediaVideoEncoder;
 import com.seu.magicfilter.widget.base.MagicBaseView;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -46,7 +50,9 @@ public class MagicCameraView extends MagicBaseView {
     private static final int RECORDING_OFF = 0;
     private static final int RECORDING_ON = 1;
     private static final int RECORDING_RESUMED = 2;
-    private static TextureMovieEncoder videoEncoder = new TextureMovieEncoder();
+    private MediaMuxerWrapper mMuxer;
+    private MediaVideoEncoder mVideoEncoder;
+
     private File outputFile;
 
     public MagicCameraView(Context context, AttributeSet attrs) {
@@ -60,7 +66,8 @@ public class MagicCameraView extends MagicBaseView {
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         super.onSurfaceCreated(gl, config);
-        recordingEnabled = videoEncoder.isRecording();
+//        recordingEnabled = videoEncoder.isRecording();
+        recordingEnabled = mMuxer != null ? mMuxer.isStarted() : true;
         if (recordingEnabled) {
             recordingStatus = RECORDING_RESUMED;
         } else {
@@ -92,14 +99,16 @@ public class MagicCameraView extends MagicBaseView {
             switch (recordingStatus) {
                 case RECORDING_OFF:
                     CameraInfo info = CameraEngine.getCameraInfo();
-                    videoEncoder.startRecording(new TextureMovieEncoder.EncoderConfig(
-                            outputFile, MagicParams.videoWidth, MagicParams.videoHeight,
-                            1000000, EGL14.eglGetCurrentContext(),
-                            info));
+//                    videoEncoder.startRecording(new TextureMovieEncoder.EncoderConfig(
+//                            outputFile, MagicParams.videoWidth, MagicParams.videoHeight,
+//                            1000000, EGL14.eglGetCurrentContext(),
+//                            info));
+                    startRecording();
+                    mVideoEncoder.setEglContext(EGL14.eglGetCurrentContext(), textureId);
                     recordingStatus = RECORDING_ON;
                     break;
                 case RECORDING_RESUMED:
-                    videoEncoder.updateSharedContext(EGL14.eglGetCurrentContext());
+//                    videoEncoder.updateSharedContext(EGL14.eglGetCurrentContext());
                     recordingStatus = RECORDING_ON;
                     break;
                 case RECORDING_ON:
@@ -111,7 +120,8 @@ public class MagicCameraView extends MagicBaseView {
             switch (recordingStatus) {
                 case RECORDING_ON:
                 case RECORDING_RESUMED:
-                    videoEncoder.stopRecording();
+//                    videoEncoder.stopRecording();
+                    stopRecording();
                     recordingStatus = RECORDING_OFF;
                     break;
                 case RECORDING_OFF:
@@ -130,8 +140,12 @@ public class MagicCameraView extends MagicBaseView {
             id = cameraInputFilter.onDrawToTexture(textureId);
             filter.onDrawFrame(id, gLCubeBuffer, gLTextureBuffer);
         }
-        videoEncoder.setTextureId(id);
-        videoEncoder.frameAvailable(surfaceTexture);
+//        videoEncoder.setTextureId(id);
+//        videoEncoder.frameAvailable(surfaceTexture);
+        if(mVideoEncoder != null) {
+            mVideoEncoder.frameAvailableSoon(id,surfaceTexture);
+        }
+
     }
 
     private SurfaceTexture.OnFrameAvailableListener onFrameAvailableListener = new SurfaceTexture.OnFrameAvailableListener() {
@@ -145,7 +159,7 @@ public class MagicCameraView extends MagicBaseView {
     @Override
     public void setFilter(MagicFilterType type) {
         super.setFilter(type);
-        videoEncoder.setFilter(type);
+//        videoEncoder.setFilter(type);
     }
 
     private void openCamera(){
@@ -164,7 +178,8 @@ public class MagicCameraView extends MagicBaseView {
                 info.isFront, true);
         gLTextureBuffer.clear();
         gLTextureBuffer.put(textureCords).position(0);
-        videoEncoder.setPreviewSize(imageWidth, imageHeight);
+//        videoEncoder.setPreviewSize(imageWidth, imageHeight);   by shichao.chen
+
         if(surfaceTexture != null)
             CameraEngine.startPreview(surfaceTexture);
     }
@@ -198,7 +213,7 @@ public class MagicCameraView extends MagicBaseView {
             @Override
             public void onPictureTaken(byte[] data, Camera camera) {
                 final Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-                if(filter != null) {
+                if (filter != null) {
                     queueEvent(new Runnable() {
                         @Override
                         public void run() {
@@ -210,10 +225,72 @@ public class MagicCameraView extends MagicBaseView {
                                 savePictureTask.execute(photo);
                         }
                     });
-                }else{
+                } else {
                     savePictureTask.execute(bitmap);
                 }
             }
         });
     }
+
+
+    /**
+     * start resorcing
+     * This is a sample project and call this on UI thread to avoid being complicated
+     * but basically this should be called on private thread because prepareing
+     * of encoder is heavy work
+     */
+    private void startRecording() {
+//        if (DEBUG) Log.v(TAG, "startRecording:");
+        try {
+//            mRecordButton.setColorFilter(0xffff0000);	// turn red
+            mMuxer = new MediaMuxerWrapper(".mp4");	// if you record audio only, ".m4a" is also OK.
+            if (true) {
+                // for video capturing
+                new MediaVideoEncoder(mMuxer, mMediaEncoderListener,MagicParams.videoWidth, MagicParams.videoHeight);
+            }
+            if (true) {
+                // for audio capturing
+                new MediaAudioEncoder(mMuxer, mMediaEncoderListener);
+            }
+            mMuxer.prepare();
+            mMuxer.startRecording();
+        } catch (final IOException e) {
+//            mRecordButton.setColorFilter(0);
+//            Log.e(TAG, "startCapture:", e);
+        }
+    }
+
+    /**
+     * request stop recording
+     */
+    private void stopRecording() {
+//        if (DEBUG) Log.v(TAG, "stopRecording:mMuxer=" + mMuxer);
+//        mRecordButton.setColorFilter(0);	// return to default color
+        if (mMuxer != null) {
+            mMuxer.stopRecording();
+            mMuxer = null;
+            // you should not wait here
+        }
+    }
+
+    /**
+     * callback methods from encoder
+     */
+    private final MediaEncoder.MediaEncoderListener mMediaEncoderListener = new MediaEncoder.MediaEncoderListener() {
+        @Override
+        public void onPrepared(final MediaEncoder encoder) {
+//            if (DEBUG) Log.v(TAG, "onPrepared:encoder=" + encoder);
+            if (encoder instanceof MediaVideoEncoder)
+//                mCameraView.setVideoEncoder((MediaVideoEncoder)encoder);
+                mVideoEncoder = (MediaVideoEncoder)encoder;
+        }
+
+        @Override
+        public void onStopped(final MediaEncoder encoder) {
+//            if (DEBUG) Log.v(TAG, "onStopped:encoder=" + encoder);
+            if (encoder instanceof MediaVideoEncoder)
+//                mCameraView.setVideoEncoder(null);
+                mVideoEncoder = (MediaVideoEncoder)encoder;
+        }
+    };
 }
